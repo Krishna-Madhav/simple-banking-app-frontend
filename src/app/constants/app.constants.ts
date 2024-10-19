140513a7046b48f2e99c5ae5f5d082f0b1cda171
Krishna-Madhav <getgoingzazz@gmail.com>
2024-10-19 13:51:00 +0200

    Environment and constants

---
+++ src/app/constants/app.constants.ts
@@ -0,0 +1,19 @@
+export const AppConstants = {
+  API_METHOD: {
+    ACCOUNTS: 'accounts',
+    DEPOSIT: 'deposit',
+    WITHDRAW: 'withdraw',
+    TRANSACTIONS: 'transactions',
+    TRANSFER: 'transfer',
+  },
+
+  TRANSACTION: {
+    MIN_BALANCE: 0,
+    DEPOSIT_SUCCESS: 'Deposit completed successfully!',
+    WITHDRAW_SUCCESS: 'Withdrawal completed successfully!',
+    TRANSFER_SUCCESS: 'Transfer completed successfully!',
+    DEPOSIT_ERROR: 'An error occurred during the deposit.',
+    WITHDRAW_ERROR: 'An error occurred during the withdrawal.',
+    TRANSFER_ERROR: 'An error occurred during the transfer.',
+  },
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiURL: 'http://localhost:8080/',
+  //   apiURL: 'https://markant.minibank.com/',  // Keeping this just for the sake of completetion as production url will be different based on domain name.
+};
